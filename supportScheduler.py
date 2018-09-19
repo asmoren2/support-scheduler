8c47ff9c3347411f187c9796fb129537e9f53365
Adolfo Moreno <adolfomoreno0@gmail.com>
2018-09-18 23:15:53 -0500

    updated print for python 3.6

--- supportScheduler.py
+++ supportScheduler.py
@@ -3,35 +3,36 @@ import pprint
 import random
 
 def getScheduleInfo():
-    single = {}
     multiple = []
-    dates = []
-    with open('support.csv') as csv_file:
+    with open('/Users/adolfomoreno/Desktop/scheduler/support-scheduler/support.csv') as csv_file:
         csv_reader = csv.DictReader(csv_file, delimiter=',')
-        line_count = 0
         for row in csv_reader:
             row['magicNumber'] = random.randint(1,101)
             multiple.append(row)
     return multiple
 
-# def getPickOrder():
-#     # grab object run through names based on rank randomize put into order
-
 def specialOrder(list):
     return sorted(list, key = lambda i: (i['Rank'], i['magicNumber']),reverse=True)
 
 def assignDate(lista):
+    dateCounter = {}
+    primary = []
+    secondary = []
     for person in lista:
         # print type(person)
-        # print person
         if int(person['Rank']) == 3:
-            print pprint.pprint(person['Name'])
-            print person.keys()[person.values().index('1')]
+            pprint.pprint(person['Name'])
+            # pprint.pprint(person.keys()[person.index('1')])
+            dateCounter[list(person.keys())[list(person.values()).index('1')]] = 1
+            pprint.pprint(list(person.keys())[list(person.values()).index('1')])
+
+            # dateList.append()
+    pprint.pprint(dateCounter)
+            # pprint.pprint(person.keys()[person.values().index('1')])
 
 
 def scheduleMe():
     # pprint.pprint(specialOrder(getScheduleInfo()))
     assignDate(specialOrder(getScheduleInfo()))
 
-scheduleMe()    
-
+scheduleMe()
